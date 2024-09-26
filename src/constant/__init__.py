524f8373f7e482c167aae6cb262d3a968870fc06
quamrul hoda <qhoda489@gmail.com>
2024-09-27 04:06:41 +0530

    completed all modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,8 @@
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "pwskills"
+MONGO_COLLECTION_NAME = "waferfault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL = "mongodb+srv://quamrul:12345@cluster0.yqyic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+artifact_folder = "artifacts"
